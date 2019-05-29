e5ce9e6cf56b79948e3f933c9f07d4c577a456da
evilamaior <contatoevilamaior@gmail.com>
2019-05-29 17:31:29 -0300

    refactored code

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
+function cpfValidator(cpf) {
+  const checkEqualDigitsAndLength = checkEqualDigits(cpf) && checkLength(cpf);
+  const isFirstDigitValid = calculateDigit(cpf, 9) === parseInt(cpf[9]);
+  const isSecondDigitValid = calculateDigit(cpf, 10) === parseInt(cpf[10]);
+  return (checkEqualDigitsAndLength && isFirstDigitValid && isSecondDigitValid);
+}
+function checkEqualDigits(cpf) {
+  return !arrayNumber(cpf).every((digit) => digit === arrayNumber(cpf)[0]);
+}
+function checkLength(cpf) {
+  return cpf.length === 11;
+}
 function arrayNumber(cpf) {
   return cpf.split('').map(Number);
 }
-function checkEqualDigits(cpf) {
-  return !(cpf === '00000000000' || cpf === '11111111111' || cpf === '22222222222' || cpf === '33333333333' || cpf === '44444444444' || cpf === '55555555555' || cpf === '66666666666' || cpf === '77777777777' || cpf === '88888888888' || cpf === '99999999999');
-}
-function checkLength(cpf) {
-    return cpf.length === 11;
+function calculateDigit(cpf, digitIndex) {
+  const sumDigits = multiplyDigits(cpf, digitIndex + 1)
+    .slice(0, digitIndex)
+    .reduce((accumulator, currentValue) => 
+      accumulator + currentValue, 0);
+  return sumDigits * 10 % 11 % 10;
 }
 function multiplyDigits(cpf, mult) {
-  return arrayNumber(cpf).map((element, i) => {
-    let multiplier = mult - i;
-    return element * multiplier;
-  });
-}
-function calculateFirstDigit(cpf) {
-  const sumDigits = multiplyDigits(cpf, 10).slice(0, 9).reduce((accumulator, currentValue) => {
-    return accumulator + currentValue;
-  });
-  return sumDigits * 10 % 11 % 10;
-}
-function calculateSecondDigit(cpf) {
-  const sumDigits = multiplyDigits(cpf, 11).slice(0, 10).reduce((accumulator, currentValue) => {
-    return accumulator + currentValue;
-  });
-  return sumDigits * 10 % 11 % 10;
-}
-function cpfValidator(cpf) {
-  return (calculateFirstDigit(cpf) === parseInt(cpf[9]) && calculateSecondDigit(cpf) === parseInt(cpf[10]) && checkEqualDigits(cpf) && checkLength(cpf));
+  return arrayNumber(cpf).map((element, i) => 
+    element * (mult - i)
+  ); 
 }
 module.exports.cpfValidator = cpfValidator;
